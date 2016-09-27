f617d609a3f2824bc33f14f6e501910c467f3396
Sam Viljoen <sam.viljoen@gmail.com>
2016-09-28 00:04:01 +0200

    main.html styling done

--- www/js/main.js
+++ www/js/main.js
@@ -80,10 +80,10 @@ function createRooms()
 				iBtn++;
 				var cell = row.insertCell(j);
 				var btn = document.createElement('div');
-
-				btn.innerHTML = "<input type = 'button' class='button button-fill' value = '"+arrStack[0]+"' onClick = 'gotoAdder(this.value)'>"; 
+				btn.setAttribute('style','text-align:left;padding-left:5%');
+				btn.innerHTML = "<div style='display: inline-block;width: 95%'> <input type = 'button' class='button button-fill' value = '"+arrStack[0]+"' onClick = 'gotoAdder(this.value)'></div>"; 
 				if (iBtn>13) {
-					btn.innerHTML += '&nbsp<img src="../img/delete.gif" alt="Delete" onclick="removeRoom('+"'"+arrStack[0]+"'"+')">'; 
+					btn.innerHTML = "<div style='display: inline-block;width: 80%'> <input type = 'button' style='vertical-align:bottom' class='button button-fill' value = '"+arrStack[0]+"' onClick = 'gotoAdder(this.value)'></div>"+'<div style="display:inline-block"><img src="../img/delete.gif" style="padding-left:60%;padding-right:40%;vertical-align:bottom" alt="Delete" onclick="removeRoom('+"'"+arrStack[0]+"'"+')"></div>'; 
 				}
 
 				cell.appendChild(btn);

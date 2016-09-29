9bd2e0540a1768c6cac088940cc6259d71e40a8b
Alwyn Grobler <alwyn.grobler@gmail.com>
2016-09-29 21:29:27 +0200

    Fixed bug where phone introduces line feeds

--- www/js/recalc.js
+++ www/js/recalc.js
@@ -5,26 +5,32 @@ function recalc()
 	if (localStorage.getItem("roomList")!=null) {
 		var roomList = localStorage.getItem("roomList");
 		var arrRooms = roomList.split(";");
+		//alert('a');
 
 		for (var i=0;i<arrRooms.length;i++) {
 			var roomDetail = localStorage.getItem(arrRooms[i]);
 			var arrRoomDetail = roomDetail.split(";");
+			//alert('b');
 
 			for (var j=0;j<arrRoomDetail.length/* && arrRoomDetail[j]!='' && arrRoomDetail[j]!=null*/;j++) {
 				var arrDetails = arrRoomDetail[j].split(",");
+				//alert('c');
 				if (arrDetails[1]!=null && arrDetails[1]!='' && arrDetails[2]!=null && arrDetails[2]!='') {
-
-					intKwh += Math.round(arrDetails[1]*arrDetails[2]*getKwh(arrDetails[0])/1000*10)/10;
+					//alert(arrDetails[0].replace("\n","")+"'");
+					//alert(getKwh(arrDetails[0].replace("\n","")));
+					intKwh += Math.round(arrDetails[1]*arrDetails[2]*getKwh(arrDetails[0].replace("\n",""))/1000*10)/10;
 				}
 			}
 		}
 
+		//alert('e');
 		localStorage.kWh = intKwh;
 	} else {
+		//alert('f');
 		localStorage.kWh = 0;
 	}
 
-	alert(localStorage.kWh);
+	
 	document.frmKwh.txtKwh.value = localStorage.kWh+" kWh";
 }
 
@@ -33,9 +39,11 @@ function getKwh(appliance)
 	var arrAppliance = localStorage.arrApplianceList.split(',');
 	var arrKwh = localStorage.arrApplianceWattList.split(',');
 	var iKwh = 0;
-
+	
 	for (var i=0;i<arrAppliance.length;i++) {
+		//alert("'"+arrAppliance[i]+"'"+appliance+"'");
 		if (arrAppliance[i]==appliance) {
+			//alert('t');
 			iKwh = arrKwh[i];
 			i = arrAppliance.length;
 		}
